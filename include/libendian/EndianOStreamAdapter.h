db388230f996a75f7bf6f94771f79126028998d2
Alexander Grund <Flamefire@users.noreply.github.com>
2025-05-22 17:07:45 +0200

    Use C++17 variable templates for traits

--- include/libendian/EndianOStreamAdapter.h
+++ include/libendian/EndianOStreamAdapter.h
@@ -1,7 +1,7 @@
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
 //
-// Copyright (C) 2005 - 2021 Settlers Freaks (sf-team at siedler25.org)
+// Copyright (C) 2005 - 2025 Settlers Freaks (sf-team at siedler25.org)
 //
 
 #pragma once
@@ -79,7 +79,7 @@ public:
     }
 
     template<typename T>
-    std::enable_if_t<std::is_arithmetic<T>::value, EndianOStreamAdapter>& operator<<(const T& value)
+    std::enable_if_t<std::is_arithmetic_v<T>, EndianOStreamAdapter>& operator<<(const T& value)
     {
         write(value);
         return *this;
